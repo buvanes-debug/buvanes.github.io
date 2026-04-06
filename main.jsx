c597cedb37355be895b6f5c17c8b6ec038af7cd7
Sidney Buvane <buvanes@africau.edu>
2026-04-06 11:13:39 +0200

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Dashboards from './dashboard.jsx'
+import './dashbords.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Dashboards />
+  </React.StrictMode>,
+)
